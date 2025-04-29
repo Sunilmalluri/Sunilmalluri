59d266d3b521638a48e1fda8e41ec8da3fab9d04
Sunilmalluri <sunilmalluri08@gmail.com>
2025-04-29 11:30:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+document.querySelector('.hamburger').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('nav-active');
+});
